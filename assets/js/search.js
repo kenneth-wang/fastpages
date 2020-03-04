b1c78deaf055f51e676774a11507ce1277966881
kenneth-wang <kenneth-wang@users.noreply.github.com>
2020-03-04 13:18:12 +0000

    deploy: 16e5371bfea59d327a0a7828323b36d04ecd694d

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://kenneth-wang.github.io/about/",
     "title": "Kenneth Wang",
-    "body": " I am a graduate of NUS Business School and consider myself as an outgoing and highly driven individual with a strong emphasis on teamwork. Currently working on pre-built AI solutions (“Bricks”) for AI Makerspace, a national platform offered by AI Singapore to help SMEs and Start-ups accelerate the adoption of AI in Singapore. Active contributor to the development of information retrieval engine and auto labeling bricks. I have a strong interest in Natural Language Processing and am currently seeking opportunities to be at the intersection of Finance and AI. Looking forward to connecting with like-minded individuals and learn something new every day! "
+    "body": "       AI Engineer (Apprentice) @ AI Singapore   Aspiring AI/ML Engineer   Looking forward to connecting with like-minded individuals and learn something new every day! "
     }, {
     "id": 2,
     "url": "https://kenneth-wang.github.io/categories/",
